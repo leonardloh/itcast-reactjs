5544686fcb0796b32d5915c3453256195e3bd636
Loh Jing Zhi <jingzhi.loh@skymind.my>
2020-06-05 22:23:07 +0800

    first commit -hello world

---
+++ src/.umi/core/routes.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import { ApplyPluginsType } from 'D:/Repos/itcast-reactjs/node_modules/@umijs/runtime';
+import { plugin } from './plugin';
+
+const routes = [
+  {
+    "path": "/helloworld",
+    "exact": true,
+    "component": require('@/pages/helloworld.js').default
+  }
+];
+
+// allow user to extend routes
+plugin.applyPlugins({
+  key: 'patchRoutes',
+  type: ApplyPluginsType.event,
+  args: { routes },
+});
+
+export { routes };
---
+++ src/pages/helloworld.js
@@ -0,0 +1,10 @@
+export default () =>{
+
+    const t = () => "jing zhi";
+    return (
+        <div>
+            <div>Hello</div>
+            <div>World {t()}</div>
+        </div>
+    );
+}
